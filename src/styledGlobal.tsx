ff20136d608c92718f37dbf3f5467a7f048b6cb9
Baranov-Dmitry <bbgggnn@gmail.com>
2023-02-03 14:41:31 +0300

    working on layout

---
+++ src/styledGlobal.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+import "./fonts/Roboto-Regular.ttf"
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+  }
+`
+
+export default GlobalStyle;
